5bd1b3d5bf230a2083f436a96b290dea7872ed74
RajeshSujin <rajesh1001sujin@gmail.com>
2024-03-05 15:29:27 +0530

    test

--- src/test/java/Identify_courses/Baseclass.java
+++ src/test/java/Identify_courses/Baseclass.java
@@ -6,13 +6,18 @@ import org.testng.annotations.Parameters;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
  
 public class Baseclass 
 {
@@ -22,17 +27,21 @@ public class Baseclass
 	//browseropen
 	@BeforeTest(groups= {"smoke","regression","master"})
 	@Parameters("browser")
-	public void launchBrowser(String Browser) throws InterruptedException {
-		switch(Browser) {
-		case "chrome":
-			driver = new ChromeDriver();
-			break;
-		case "edge":
-			driver=new EdgeDriver();
-			break;
-		default:
-			System.out.println("No matching browser");
-		}
+	public void launchBrowser(String Browser) throws InterruptedException, MalformedURLException {
+//		switch(Browser) {
+//		case "chrome":
+//			driver = new ChromeDriver();
+//			break;
+//		case "edge":
+//			driver=new EdgeDriver();
+//			break;
+//		default:
+//			System.out.println("No matching browser");
+//		}
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setPlatform(Platform.WIN11);
+		capabilities.setBrowserName("MicrosoftEdge");
+		driver = new RemoteWebDriver(new URL("http://192.168.138.4:4444"), capabilities);
 		driver.manage().deleteAllCookies();
 		driver.get("https://www.coursera.org/");
 		//Thread.sleep(20000);
